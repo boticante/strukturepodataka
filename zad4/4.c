78adc5eba415031b7e698ad2c58f4f878f368c52
Ante Botić <115645763+boticante@users.noreply.github.com>
2023-11-13 13:26:30 +0100

    Update 4.c

--- zad4/4.c
+++ zad4/4.c
@@ -4,262 +4,218 @@
 Napomena: Eksponenti u datoteci nisu nužno sortirani.
 */
 
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define MAX_LENGTH 50
-
-typedef struct Poly* Position;
-struct Poly {
+typedef struct _Poly* Position;
+typedef struct _Poly {
     int coefficient;
     int exponent;
     Position next;
-};
+}Poly;
 
-int readFromFile(Position);
-int printPoly(Position);
-int sort(Position);
-Position sumPoly(Position, Position);
-Position multiplyPoly(Position, Position);
+int readPolyFromFile(char filename[], Position poly1, Position poly2);
+int addToList(Position poly, Position newElement);
+Position createElement(int coefficient, int exponent);
+int sumPoly(Position poly1, Position poly2, Position sumResult);
+int multiplyPoly(Position poly1, Position poly2, Position multiplyResult);
+int printList(Position poly);
 
-int main()
-{
-    struct Poly p1;
-    struct Poly p2;
+int main() {
 
-    readFromFile(&p1);
-    sort(&p1);
-    printf("ispis prvog polinoma: \n");
-    printPoly(p1.next);
+    Poly poly1 = {.coefficient = 0, .exponent = 0, .next = NULL};
+    Poly poly2 = {.coefficient = 0, .exponent = 0, .next = NULL};
+    Poly multiply = {.coefficient = 0, .exponent = 0, .next = NULL};
+    Poly sum = {.coefficient = 0, .exponent = 0, .next = NULL};
+    char filename[50];
 
-    printf("\n");
+    printf("Unesite ime dokumenta iz kojeg citamo polinome: ");
+    scanf(" %[^\n]", filename);
 
-    readFromFile(&p2);
-    sort(&p2);
-    printf("ispis drugog polinoma: \n");
-    printPoly(p2.next);
+    readPolyFromFile(filename, &poly1, &poly2);
 
-    printf("\n");
+    multiplyPoly(&poly1, &poly2, &multiply);
 
-    Position sumResult = sumPoly(&p1, &p2);
-    printf("Zbroj polinoma je:\n");
-    printPoly(sumResult->next);
+    printf("\nPolinom 1: ");
+    printList(&poly1);
+    printf("\nPolinom 2: ");
+    printList(&poly2);
 
-    printf("\n");
+    sumPoly(&poly1, &poly2, &sum);
 
-    Position multiplyResult = multiplyPoly(&p1, &p2);
-    printf("Umnozak polinoma je:\n");
-    printPoly(multiplyResult->next);
-
-    return EXIT_SUCCESS;
-}
-
-int readFromFile(Position P)
-{
-    char filename[MAX_LENGTH];
-    P->next = NULL;
-
-    printf("Unesite ime datoteke iz koje zelite ucitat polinom: ");
-    scanf(" %s", &filename);
-    FILE* filePointer = NULL;
-    filePointer = fopen(filename, "r");
-    
-    if (!filePointer)
-    {
-        printf("Datoteka ne postoji!\n");
-        return;
-    }
-
-    while (!feof(filePointer))
-    {
-        Position current = NULL;
-        current = (Position)malloc(sizeof(struct Poly));
-        
-        if (!current)
-        {
-            printf("Greska u alokaciji memorije!\n");
-        }
-        fscanf(filePointer, "%d %d", &current->coefficient, &current->exponent);
-        
-        current->next = P->next;
-        P->next = current;
-        P = P->next;
-    }
-    
-    fclose(filePointer);
-    printf("Lista ucitana iz datoteke, ");
-
-    return EXIT_SUCCESS;
-}
-
-int printPoly(Position P) 
-{
-    printf("p(x) = ");
-    while (P != NULL) 
-    {
-        printf("%c%dx^%d ", P->coefficient > 0 ? '+' : ' ', P->coefficient, P->exponent);
-        P = P->next;
-    }
+    printf("\n-------Mnozenje--------\n");
+    printList(&multiply);
+    printf("\n-------Zbrajanje-------\n");
+    printList(&sum);
     printf("\n");
 
     return EXIT_SUCCESS;
 }
 
-int sort(Position P) {
-    Position new = NULL;
-    Position previous = NULL;
-    Position temporary = NULL;
-    Position last = NULL;
+int readPolyFromFile(char filename[], Position poly1, Position poly2) 
+{
+    FILE* fp = NULL;
+    int coefficient = 0;
+    int exponent = 0;
+    int numberOfBytes = 0;
+    char* line = NULL;
+    char staticLine[1024] = {0};
+    Position newElement = NULL;
 
-    while (P->next != last)
+    fp = fopen(filename, "r");
+
+    if(!fp) 
     {
-        previous = P;
-        new = P->next;
-        while (new->next != last)
-        {
-            if (new->exponent > new->next->exponent)
-            {
-                temporary = new->next;
-                previous->next = temporary;
-                new->next = temporary->next;
-                temporary->next = new;
-                new = temporary;
-            }
-            previous = new;
-            new = new->next;
-        }
-        last = new;
+        printf("Dokument se ne moze otvoriti!\n");
+        return EXIT_FAILURE;
     }
+
+    line = (char*)malloc(1024 * sizeof(char));
+
+    if(!line) 
+    {
+        printf("Memorija za redak se ne moze alocirati!\n");
+        return EXIT_FAILURE;
+    }
+
+    fgets(staticLine, 1024, fp);
+    line = staticLine;
+
+    while(strlen(line) > 0) 
+    {
+        sscanf(line, "%d %d %n", &coefficient, &exponent, &numberOfBytes);
+
+        if(coefficient != 0) 
+        {
+            newElement = createElement(coefficient, exponent);
+            addToList(poly1, newElement);
+        }
+        line += numberOfBytes;
+    }
+
+    fgets(staticLine, 1024, fp);
+    line = staticLine;
+
+    while(strlen(line) > 0) 
+    {
+        sscanf(line, "%d %d %n", &coefficient, &exponent, &numberOfBytes);
+        
+        if(coefficient != 0)
+        {
+            newElement = createElement(coefficient, exponent);
+            addToList(poly2, newElement);
+        }
+        line += numberOfBytes;
+    }
+    fclose(fp);
 
     return EXIT_SUCCESS;
 }
 
-Position sumPoly(Position p1, Position p2) 
+Position createElement(int coefficient, int exponent) 
 {
-    Position sumResult = NULL;
-    sumResult = (Position)malloc(sizeof(struct Poly));
-    
-    if (!sumResult) 
+    Position newElement = NULL;
+    newElement = (Position)malloc(sizeof(Poly));
+
+    if(!newElement) 
     {
-        printf("Greska u alokaciji memorije prilikom zbrajanja polinoma!");
-        return EXIT_SUCCESS;
-    }
-    sumResult->next = NULL;
-
-    p1 = p1->next; 
-    p2 = p2->next;
-    Position currentResult = sumResult;
-
-    while (p1 != NULL || p2 != NULL) 
-    {
-        Position new = NULL;
-        new = (Position)malloc(sizeof(struct Poly));
-        
-        if (!new) 
-        {
-            printf("Greska u alokaciji memorije prilikom zbrajanja polinoma!");
-            return EXIT_SUCCESS;
-        }
-        
-        if (p1 == NULL) 
-        {
-            new->exponent = p2->exponent;
-            new->coefficient = p2->coefficient;
-            p2 = p2->next;
-        }
-        else if (p2 == NULL) 
-        {
-            new->exponent = p1->exponent;
-            new->coefficient = p1->coefficient;
-            p1 = p1->next;
-        }
-        else if (p1->exponent == p2->exponent) 
-        {
-            new->exponent = p1->exponent;
-            new->coefficient = p1->coefficient + p2->coefficient;
-            p1 = p1->next;
-            p2 = p2->next;
-        }
-        else if (p1->exponent > p2->exponent) 
-        {
-            new->exponent = p2->exponent;
-            new->coefficient = p2->coefficient;
-            p2 = p2->next;
-        }
-        else if (p1->exponent < p2->exponent) 
-        {
-            new->exponent = p1->exponent;
-            new->coefficient = p1->coefficient;
-            p1 = p1->next;
-        }
-
-        new->next = NULL;
-        currentResult->next = new;
-        currentResult = currentResult->next;
-    }
-
-    return sumResult;
-}
-
-Position multiplyPoly(Position p1, Position p2) 
-{
-    p1 = p1->next; 
-    p2 = p2->next;
-    Position multiplyResult = NULL;
-    
-    multiplyResult = (Position)malloc(sizeof(struct Poly));
-    
-    if (!multiplyResult) 
-    {
-        printf("Greska u alokaciji memorije prilikom mnozenja polinoma!");
+        printf("Memorija za novi element se ne moze alocirati!\n");
         return NULL;
     }
-    multiplyResult->next = NULL;
 
-    Position headPoly2 = p2;
+    newElement->coefficient = coefficient;
+    newElement->exponent = exponent;
+    newElement->next = NULL;
+    
+    return newElement;
+}
 
-    while (p1 != NULL) 
+int addToList(Position poly, Position newElement) {
+
+    if(poly->next == NULL)
+        poly->next = newElement;
+    else 
     {
-        Position productPoly = NULL;
-        productPoly = (Position)malloc(sizeof(struct Poly));
-        
-        if (!productPoly) 
+        while(poly->next != NULL && poly->next->exponent > newElement->exponent)
+            poly = poly->next;
+
+        if(poly->next != NULL && poly->next->exponent == newElement->exponent)
         {
-            printf("Greska u alokaciji memorije prilikom mnozenja polinoma!");
-            return EXIT_SUCCESS;
+            poly->next->coefficient += newElement->coefficient;  
+            free(newElement);
         }
-        productPoly->next = NULL;
-
-        Position currentResult = productPoly;
-
-        while (p2 != NULL) 
+        else
         {
-            Position new = NULL;
-            new = (Position)malloc(sizeof(struct Poly));
-            
-            if (!new) 
-            {
-                printf("Greska u alokaciji memorije prilikom mnozenja polinoma!");
-                return EXIT_SUCCESS;
-            }
-
-            new->exponent = p1->exponent + p2->exponent;
-            new->coefficient = p1->coefficient * p2->coefficient;
-            new->next = NULL;
-            currentResult->next = new;
-            currentResult = currentResult->next;
-            p2 = p2->next;
+            newElement->next = poly->next;
+            poly->next = newElement;
         }
-
-        Position newProductPoly = sumPoly(productPoly, multiplyResult);
-        free(multiplyResult);
-        multiplyResult = newProductPoly;
-
-        p1 = p1->next;
-        p2 = headPoly2;
     }
 
-    return multiplyResult;
+    return EXIT_SUCCESS;
 }
+
+int printList(Position poly) 
+{
+    while (poly->next != NULL) 
+    {
+        poly = poly->next;
+       
+        if(poly->next == NULL) 
+        {
+            if(poly->exponent == 0)
+                printf("%d", poly->coefficient);
+            else
+                printf("%dx^%d", poly->coefficient, poly->exponent);
+        }
+        else if(poly->exponent == 0)
+            printf("%d + ", poly->coefficient);
+        else
+            printf("%dx^%d + ", poly->coefficient, poly->exponent);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int multiplyPoly(Position poly1, Position poly2, Position multiplyResult) 
+{
+    Position newElement = NULL;
+    Position privremena = poly2;
+
+    while(poly1->next != NULL) 
+    { 
+        poly1 = poly1->next;
+        poly2 = privremena;
+
+        while(poly2->next != NULL) 
+        {
+            poly2 = poly2->next;
+            newElement = createElement(poly1->coefficient * poly2->coefficient, poly1->exponent + poly2->exponent);
+            addToList(multiplyResult, newElement);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
+
+ int sumPoly(Position poly1, Position poly2, Position sumResult)
+ {  
+    Position temp;
+    temp = poly1;
+     
+    while(temp->next)
+    {
+        addToList(sumResult, createElement(temp->next->coefficient, temp->next->exponent));
+        temp = temp->next;
+    }
+     
+    temp = poly2;
+     
+    while(temp->next)
+    {
+        addToList(sumResult, createElement(temp->next->coefficient, temp->next->exponent));
+        temp = temp->next;
+    }
+     
+    return EXIT_SUCCESS;
+ }

089ce22ac2d72282a3b2270985df9ffa1c5be347
Ante Botić <115645763+boticante@users.noreply.github.com>
2024-01-07 02:30:32 +0100

    Update 8.c

--- 8.c
+++ 8.c
@@ -20,8 +20,11 @@ treePointer AllocateNew(int);
 int InOrder(treePointer);
 int PostOrder(treePointer);
 int PreOrder(treePointer);
+int LevelOrder(treePointer);
 int Height(treePointer);
 int CurrentLevel(treePointer, int );
+treePointer DeleteElement(int, treePointer);
+treePointer FindMin(treePointer);
 treePointer FindElement(int, treePointer);
 
 int main()
@@ -37,8 +40,10 @@ int main()
 		printf("2 - ispis inorder stabla\n");
 		printf("3 - ispis postorder stabla\n");
 		printf("4 - ispis preorder stabla\n");
-		printf("5 - trazenje elementa stabla\n");
-		printf("6 - izlaz\n");
+		printf("5 - ispis level order stabla\n");
+		printf("6 - trazenje elementa stabla\n");
+		printf("7 - brisanje elementa iz stabla\n");
+		printf("8 - izlaz\n");
 		printf("-----\n");
 		printf("> ");
 		scanf("%d", &choice);
@@ -77,6 +82,14 @@ int main()
             }
 			break;
 		case 5:
+            if (root == NULL)
+                printf("Stablo je prazno.\n");
+            else {
+			    printf("Ispis LevelOrder: ");
+			    LevelOrder(root);
+            }
+			break;
+		case 6:
 			printf("Unesi element koji trazis u stablu: ");
 			scanf("%d", &element);
 			temp = FindElement(element, root);
@@ -85,7 +98,12 @@ int main()
             else
 				printf("Element %d se nalazi na adresi %p\n", temp->value, temp);
 			break;
-        case 6:
+		case 7:
+			printf("Unesi element koji brises iz stabla: ");
+			scanf("%d", &element);
+			root = DeleteElement(element, root);
+			break;
+        case 8:
             return 0;
         }
 		printf("\n");
@@ -154,6 +172,16 @@ int PreOrder(treePointer root)
 	return 0;
 }
 
+int LevelOrder(treePointer root)
+{
+    int h = Height(root);
+    int i;
+    for (i = 1; i <= h; i++) {
+        CurrentLevel(root, i);
+	}
+	return 0;
+}
+
 int CurrentLevel(treePointer root, int level)
 {
     if (root == NULL) {
@@ -180,6 +208,45 @@ int Height(treePointer root)
         return (lheight + 1);
     else
         return (rheight + 1);
+}
+
+treePointer DeleteElement(int element, treePointer root)
+{
+	treePointer temp;
+	if (root==NULL) {
+		return root;
+    }
+	else if (element < root->value) {
+		root->left = DeleteElement(element, root->left);
+    }
+	else if (element > root->value) {
+		root->right = DeleteElement(element, root->right);
+    }
+	else {
+		if (root->left == NULL)
+		{
+			temp = root->right;
+			free(root);
+			return temp;
+		}
+		else if(root->right==NULL)
+		{
+			temp = root->left;
+			free(root);
+			return temp;
+		}
+		temp=FindMin(root->right);
+		root->value=temp->value;
+		root->right=DeleteElement(temp->value,root->right);
+	}
+	return root;
+}
+
+treePointer FindMin(treePointer root)
+{
+	while (root->left)
+		root = root->left;
+	return root;
 }
 
 treePointer FindElement(int element, treePointer root)
